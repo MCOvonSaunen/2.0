0328f48d659842aac4c2f3c14d01f1e8c6c2d49e
Marvin <marvin.opelt@outlook.com>
2026-04-01 13:32:24 +0200

    Aufgabe 3 Steckbrief

---
+++ 1_Java_Basics/Aufgabe03_Steckbrief/Steckbrief.java
@@ -0,0 +1,22 @@
+public class Steckbrief {
+    public static void main(String[] args) {
+        int alter = 19;
+        int geburtsjahr = 2006;
+        int gehalt = 2200;
+        String handynummer = "0660 3216850";
+        float körpergröße = 1.84;
+        float gewicht = 95;
+        boolean führerschein = true;
+        char note = "A";
+
+        System.out.println("=== Mein Steckbrief ===");
+        System.out.println("Alter: " + alter + " Jahre");
+        System.out.println("Geburtsjahr: " + geburtsjahr);
+        System.out.println("Gehalt: " + gehalt + "€");
+        System.out.println("Handynummer: " + handynummer);
+        System.out.println("Körpergröße: " + körpergröße);
+        System.out.println("Gewicht: " + körpergröße);
+        System.out.println("Führerschein: " + führerschein);
+        System.out.println("Lieblinbgsnote: " + note);
+    }
+}
