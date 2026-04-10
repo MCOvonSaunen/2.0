722706f2f52848ffc5e6bce96d5356e841278fd3
Marvin <marvin.opelt@outlook.com>
2026-04-10 22:48:24 +0200

    Aufgabe 6 Geschichten

--- 1_Java_Basics/Aufgabe06_Geschichten/Geschichten.java
+++ 1_Java_Basics/Aufgabe06_Geschichten/Geschichten.java
@@ -8,6 +8,6 @@ public class Geschichten {
         String name = scanner.nextLine();
         System.out.println("Was ist sein / ihr Job?");
         String job = scanner.nextLine();
-        System.out.println("Hier ist die Geschichte: "+"\n"+"Es war einmal "+ name +", ein " + job +"."+"\n"+"Auf dem Weg zum Arbeit dachte "+ name + ", über das Leben nach."+"\n"+"Vielleicht wird "+ name + " nicht für immer ein " + job + " sein.");
+        System.out.println("Hier ist die Geschichte: "+"\n"+"Es war einmal "+ name +", ein " + job +"."+"\n"+"Auf dem Weg zur Arbeit dachte "+ name + ", über das Leben nach."+"\n"+"Vielleicht wird "+ name + " nicht für immer ein " + job + " sein.");
     }
 }   
