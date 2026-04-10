b5f030e146b16ac6294ad98bb47f0bc5f6f0b2f4
Marvin <marvin.opelt@outlook.com>
2026-04-10 21:31:51 +0200

    Erstellen Arithmetische_Operatoren

---
+++ 1_Java_Basics/Aufgabe04_Arithmetische_Operatoren/Arithmetische_Operatore.java
@@ -0,0 +1,12 @@
+public class Arithmetische_Operatoren {
+    public static void main(String[] args) {
+        int y = 13;
+        int x = 14;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+    }
+}
