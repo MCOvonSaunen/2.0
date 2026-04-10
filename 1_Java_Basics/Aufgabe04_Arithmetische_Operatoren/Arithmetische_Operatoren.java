3b5ef50b282d0503a25486eff4041f932f0263f7
Marvin <marvin.opelt@outlook.com>
2026-04-10 22:08:14 +0200

    Aufgabe04_Arithmetische_Operatoren

--- 1_Java_Basics/Aufgabe04_Arithmetische_Operatoren/Arithmetische_Operatoren.java
+++ 1_Java_Basics/Aufgabe04_Arithmetische_Operatoren/Arithmetische_Operatoren.java
@@ -3,10 +3,15 @@ public class Arithmetische_Operatoren {
         int y = 13;
         int x = 14;
 
-        System.out.println(x + y);
-        System.out.println(x - y);
-        System.out.println(x * y);
-        System.out.println(x / y);
-        System.out.println(x % y);
+        System.out.println(x + y); //24
+        System.out.println(x - y); //1
+        System.out.println(x * y); //182
+        System.out.println((float)x / y); //1.0769
+        System.out.println((float)x % (float)y); //1.0769
+
+        int z = 25;
+
+        System.out.println(++z); //26
+        System.out.println(--z); //24
     }
 }
