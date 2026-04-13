0e0b01d64be9778006b266dd16f413de6307ac9d
Marvin <marvin.opelt@outlook.com>
2026-04-13 18:33:18 +0200

    Aufgabe 7 Radarstrafe

---
+++ 1_Java_Basics/Aufgabe07_Radarstrafe/Radarstrafe.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Radarstrafe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wie schnell fährst du (in km/h)?");
+        String eingabe = scanner.nextLine();
+        float kmh = Float.parseFloat(eingabe);
+        if (kmh < 70) {
+            System.out.println("Schönen Tag noch fahren sie weiter...") 
+        }
+        if (kmh >= 71) {
+            System.out.println("RADARSTRAFE!")
+        }
+    }
+}
